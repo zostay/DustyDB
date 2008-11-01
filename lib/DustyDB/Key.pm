e991c08633ed4aef9f6c54d7eb0b9702b9383a73
sterling <sterling@81bc576e-d5df-0310-b9f0-ecc7a9e07b1a>
2008-11-01 04:02:46 +0000

    Adding a stringify attribute and peform_stringify() method
    git-svn-id: http://svn.contentment.org/svn/trunk/DustyDB@980 81bc576e-d5df-0310-b9f0-ecc7a9e07b1a

--- lib/DustyDB/Key.pm
+++ lib/DustyDB/Key.pm
@@ -19,6 +19,42 @@ DustyDB::Key - mark an attribute as being part of the primary key
 
 This is a basic marker role that just notifies DustyDB that the attribute should be used to define the primary key (one of the attributes that uniquely identifies it) for the object.
 
+=head1 ATTRIBUTES
+
+=head2 stringify
+
+This may be defined as a reference to a subroutine to be used to translate a non-scalar attribute into a scalar key value. For example, if you want to use a date via L<DateTime> as a key, you could do:
+
+  has timestamp => (
+     is => 'rw',
+     isa => 'DateTime',
+     traits => [ 'DustyDB::Key' ],
+     stringify => sub { $_->iso8601 },
+  );
+
+You may also want to use this in combination with L<DustyDB::Filter> to provide encoders and decoders for your object.
+
 =cut
+
+has stringify => (
+    is => 'rw',
+    isa => 'CodeRef',
+    default => sub { sub { $_ } },
+);
+
+=head1 METHODS
+
+=head2 perform_stringify
+
+This is a helper for executing the code reference in the L</stringify> attribute correctly.
+
+=cut
+
+sub perform_stringify {
+    my ($self, $value) = @_;
+
+    local $_ = $value;
+    return $self->stringify->($value);
+}
 
 1;
