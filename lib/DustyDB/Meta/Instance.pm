6cdcb3099a31a5b938ff5e6d8d4cf869baa3de81
sterling <sterling@81bc576e-d5df-0310-b9f0-ecc7a9e07b1a>
2008-11-09 04:24:53 +0000

    Adding documentation to the instance metaclass
    git-svn-id: http://svn.contentment.org/svn/trunk/DustyDB@1018 81bc576e-d5df-0310-b9f0-ecc7a9e07b1a

--- lib/DustyDB/Meta/Instance.pm
+++ lib/DustyDB/Meta/Instance.pm
@@ -3,9 +3,19 @@ use Moose::Role;
 
 use Scalar::Util qw( reftype );
 
+=head1 NAME
+
+DustyDB::Meta::Instance - helper for auto-vivifying model links on demand
+
+=head1 DESCRIPTION
+
+Do not use this class directly. Just by using L<DustyDB::Object>, you have done everything you need to work with this class.
+
+=head1 METHODS
+
 =head2 get_slot_value
 
-This is enhanced to perform deferred loading of FK objects.
+This method has been enhanced to perform deferred loading of FK objects.
 
 =cut
 
@@ -20,6 +30,12 @@ override get_slot_value => sub {
 
     return $value;
 };
+
+=head2 inline_get_slot_value
+
+This method has been enhanced to performed deferred loading of FK object in inline accessor code.
+
+=cut
 
 override inline_get_slot_value => sub {
     my ($instance, $struct, $name) = @_;
